9b7623535e5b5c4b9648da9306962bd3e03b809a
Nimna Ekanayaka <nimna@nimna.me>
2016-05-26 13:48:16 -0300

    Trying to set yellow.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var red = [0, 100, 63];
 var orange = [40, 100, 60];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
-var yellow = [255, 255, 0];
+var yellow = [80, 74, 200];
 var purple = [280, 50, 60];
 
 var myName = "Nimna";
