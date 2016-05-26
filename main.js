2e41b1a1f909ac5e1ae8f315eb9bf61a459d8489
Nimna Ekanayaka <nimna@nimna.me>
2016-05-26 13:45:21 -0300

    Defined yellow.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var red = [0, 100, 63];
 var orange = [40, 100, 60];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
+var yellow = [255, 255, 0];
 var purple = [280, 50, 60];
 
 var myName = "Nimna";
