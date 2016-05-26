59ae3f2083bf619f326de2ac0444c866db5fc7f3
Nimna Ekanayaka <nimna@nimna.me>
2016-05-26 15:04:00 -0300

    Testing colors.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var red = [0, 100, 63];
 var orange = [40, 100, 60];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
-var yellow = [80, 74, 200];
+var yellow = [200, 74, 96];
 var purple = [280, 50, 60];
 
 var myName = "Nimna";
